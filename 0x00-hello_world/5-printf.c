f732c5179fa5a109e4e6435aa365ebe78a6eac8b
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-10-05 04:35:29 -0700

    we are now using printf to print the output value

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the beginning of all C functions
+ *
+ * Return: Always end the program to compile the code
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
