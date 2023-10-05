3ea338c5b4f5b89a8ee77ebc633ce3c318b40221
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-10-05 04:20:37 -0700

    printing ou a string using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - the function that allows the code to run in C
+ *
+ * Return: Signifies the end of code so it can be compiled
+ */
 
 int main(void)
 {
-	char statement[] = "Programming is like building a multilingual puzzle";
-	puts(statement);
+	puts("\"Programming is like building a multilingual puzzle);
 
-	return 0;
+	return (0);
+}
