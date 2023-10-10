3f2c7af9321f26df1799893226d0121c75e24179
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-10-10 05:08:33 -0700

    printing putchar with putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the beginningand main function of C code
+ *
+ * Return: return 0 after successfully execution
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
