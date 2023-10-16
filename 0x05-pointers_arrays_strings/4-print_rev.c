9bc72b32d07955ce039195078393d8682c69a141
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-10-16 13:20:50 -0700

    prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,5 @@
 #include "main.h"
-<<<<<<< HEAD
 
-/**
- * print_rev - imprime en reversa
- * @s: string
- * return: 0 when successful
- */
-void print_rev(char *s)
-=======
 /**
  * print_rev - imprime en reversa
  * @s: string
@@ -15,7 +7,6 @@ void print_rev(char *s)
  */
 void print_rev(char *s)
 
->>>>>>> edc2ded04026d5382b600f718f27a293d137631e
 {
 	int len = 0;
 
@@ -27,36 +18,20 @@ void print_rev(char *s)
 		len++;
 
 		s++;
-<<<<<<< HEAD
-=======
 
->>>>>>> edc2ded04026d5382b600f718f27a293d137631e
 	}
 
 	s--;
 
 	for (o = len; o > 0; o--)
-<<<<<<< HEAD
-	
+
 	{
 		_putchar(*s);
 
 		s--;
 
-=======
-
-	{
-		_putchar(*s);
-
->>>>>>> edc2ded04026d5382b600f718f27a293d137631e
 	}
 
 	_putchar('\n');
 
 }
-<<<<<<< HEAD
-
-	
-
-=======
->>>>>>> edc2ded04026d5382b600f718f27a293d137631e
