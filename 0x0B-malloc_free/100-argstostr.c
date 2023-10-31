5973184af5d0b3cc6c819f39e588226466ad88fb
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-10-31 02:51:03 -0700

    Write a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,39 +11,35 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int uu = 0;
-	int oo = 0;
-	int xx = 0;
-	int yy = 0;
-
-	char *string;
+	int uu, oo, ll = 0, final_length = 0;
+	char *combined_string;
 
 	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (uu = 0; uu < ac; uu++)
+	{
+		for (oo = 0; av[uu][oo]; oo++)
+			final_length++;
+
+	}
+	final_length += ac;
+
+	combined_string = malloc(sizeof(char) * final_length + 1);
+	if (combined_string == NULL)
 		return (NULL);
 
 	for (uu = 0; uu < ac; uu++)
 	{
 		for (oo = 0; av[uu][oo]; oo++)
-			yy++;
-
+		{
+			combined_string[ll] = av[uu][oo];
+			ll++;
+		}
+		if (combined_string[ll] == '\0')
+		{
+			combined_string[ll++] = '\n';
+		}
 	}
-	yy += ac;
+	return (combined_string);
 
-	string = malloc(sizeof(char) * yy + 1);
-
-	if (string == NULL)
-		return (NULL);
-
-	for (uu = 0; uu < ac; uu++)
-	{
-		string[xx] = av[uu][oo];
-
-		xx++;
-
-	}
-	if (string[xx] == '\0')
-	{
-		string[xx++] = '\n';
-	}
-	return (string);
 }
