4d9c2c7968dd20c21b746a21ebf3ced717c1c8e6
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-11-16 03:00:06 -0800

    committing everything

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,44 @@
  */
 
 int _atoi(char *s)
+
 {
 	int i = 0;
+	int d = 0;
 	int n = 0;
-	int is_negative = 0;
+	int len = 0;
+	int f = 0;
+	int digit = 0; 
 
-	if (s[0] == '-')
-	{
-		is_negative = 1;
-		i = 1;
-	}
+	while (s[len] != '\0')
 
-	while (s[i])
+		len++;
+
+	while (i < len && f == 0)
+
 	{
+		if (s[i] == '-')
+
+			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
-		{
-			int digit = s[i] - '0';
 
-			if (!is_negative && (n > INT_MAX / 10 || (n == INT_MAX / 10 && digit > 7)))
-				return (INT_MAX);
-			if (is_negative && (n < INT_MIN / 10 || (n == INT_MIN / 10 && digit > 8)))
-				return (INT_MIN);
+		{
+			digit = s[i] - '0';
+
+			if (d % 2)
+
+				digit = -digit;
 
 			n = n * 10 + digit;
 
-		}
-		else
-		{
-			break;
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+
+			f = 0;
 
 		}
 
@@ -43,6 +53,10 @@ int _atoi(char *s)
 
 	}
 
-	return (is_negative ? -n : n);
+	if (f == 0)
+
+		return (0);
+
+	return (n);
 
 }
--- 0x09_static_lib.sh/README.md
+++
@@ -1,1 +0,0 @@
-I am now doing static libraries
