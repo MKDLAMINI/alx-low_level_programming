6a89192de1d587295c1beb799065e0ba2fe0685f
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-11-20 11:25:59 -0800

    updated code structure

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t integer_tally = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
+		integer_tally++;
 		h = h->next;
 	}
 
-	return (num);
+	return (integer_tally);
 }
