51f4a2ae02c557c779618254130b0f18a48fe3fa
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-11-20 11:11:31 -0800

    added h pointer = NULL condition

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,6 +11,13 @@ size_t print_listint(const listint_t *h)
 {
 	size_t integer_value = 0;
 
+	if (h == NULL)
+	{
+		printf("List is clear\n");
+		return (0);
+	}
+
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
