e696144dc445a89f278d12d1cdbab500b19d6d3e
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-02-02 10:41:22 -0800

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,19 +10,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
-	hash_node_t *bucket;
+	hash_node_t *unlock;
 
 	if (!ht || !key || !*key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	bucket = ht->array[index];
+	unlock = ht->array[index];
 
-	while (bucket)
+	while (unlock)
 	{
-		if (!strcmp(key, bucket->key))
-			return (bucket->value);
-		bucket = bucket->next;
+		if (!strcmp(key, unlock->key))
+			return (unlock->value);
+		unlock = unlock->next;
 	}
 	return (NULL);
 }
