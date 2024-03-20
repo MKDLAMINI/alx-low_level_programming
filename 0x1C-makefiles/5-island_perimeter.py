14e50e594a9023e09808d3a57f01c58a72777a0c
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-03-20 07:41:49 -0700

    return the perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,64 +1,30 @@
 #!/usr/bin/python3
-
-"""
-island_perimeter(grid):
-
-This function calculates the perimeter of the island described in a 2D grid
-
-Args:
-    grid: A list of lists of integers representing the grid.
-    - 0: Water zone
-    - 1: Land zone
-
-
-Returns:
-    The perimeter of the island as an integer.
-
-Raises:
-    ValueError: If the grid is not rectangular or exceeds dimensions (100x100).
-"""
+"""Defines an island perimeter measuring function."""
 
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island in the provided grid.
+    """Return the perimeter of an island.
 
-    The grid is assumed to be completely surrounded by water and contain
-    only one island (or no island at all). The island doesn't have internal
-    "lakes" (water zones disconnected from the surrounding water).
+    The grid repressents water by 0 and land by 1.
 
-    Args:
-        grid: A list of lists of integers representing the grid.
-              - 0: Water zone
-              - 1: Land zone
+    Arg:
+        grid: (list): A list of list of integers representing an island.
 
     Returns:
-        The perimeter of the island as an integer.
-
-    Raises:
-        ValueError: If the grid is not rectangular or exceeds dimensions (100x100).
+        The perimeter of the island defined in grid.
     """
-
-    rows, cols = len(grid), len(grid[0])
-
-
-    if rows > 100 or cols > 100:
-        raise ValueError("Grid dimensions exceed maximum (100x100)")
-    for row in grid:
-        if len(row) != cols:
-            raise VlueError("Grid is not rectangular")
-
-    perimeter = 0
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
 
-                if row > 0 and grid[row - 1][col] == 0:
-                    perimeter -= 2
-                if col > 0 and grid[row][col - 1] == 0:
-                    perimeter -= 2
-
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                   edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
